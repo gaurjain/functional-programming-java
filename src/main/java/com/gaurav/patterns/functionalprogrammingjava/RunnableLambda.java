8d4f50ccdb7b40331079141ae93881e44e83fe95
Gaurav P Jain <gaurav.p.jain1@aexp.com>
2020-12-03 14:45:52 -0700

    Runnable interface implemented in two distinct ways 1. by implementing the run method in an anonymous class, inline. 2. with a lambda expression inline

--- src/main/java/com/gaurav/patterns/functionalprogrammingjava/RunnableLambda.java
+++ src/main/java/com/gaurav/patterns/functionalprogrammingjava/RunnableLambda.java
@@ -1,4 +1,43 @@
 package com.gaurav.patterns.functionalprogrammingjava;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RunnableLambda {
+
+    // Anonymous class definition of the Runnable interface.
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Hello world from the thread runnable [" + Thread.currentThread().getName() + "] ");
+            }
+
+        }
+
+    };
+
+    // Lambda Expression definition of the Runnable interface.
+    Runnable runnableLambda = () -> { for (int i = 0; i < 3; i++) {
+            System.out.println("Hello world from the thread runnableLambda [" + Thread.currentThread().getName() + "] "); }
+    };
+
+    //Core calling logic.
+    {
+        System.out.println("From the thread :[" + Thread.currentThread().getName() + "]");
+        Thread thread1 = new Thread(runnable);
+        thread1.start();
+        Thread thread2 = new Thread(runnableLambda);
+        thread2.start();
+        try {
+             thread1.join();
+             thread2.join();
+             System.out.println("Back to the thread :[" + Thread.currentThread().getName() + "]");
+        } catch (InterruptedException e) {
+            System.out.println("from the thread ["+ Thread.currentThread().getName() + "]: " + e);
+        }
+    }
+
+
+
 }
