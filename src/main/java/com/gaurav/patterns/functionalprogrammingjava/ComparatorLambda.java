c0776151070abb28586328d8b3b495709b48a721
Gaurav P Jain <gaurav.p.jain1@aexp.com>
2020-12-03 17:27:49 -0700

    Comparator functional interface implementation in two ways 1. with Anonymous class expecting two parameters for the compare method. 2. with lambda expression expecting two input parameters.

---
+++ src/main/java/com/gaurav/patterns/functionalprogrammingjava/ComparatorLambda.java
@@ -0,0 +1,34 @@
+package com.gaurav.patterns.functionalprogrammingjava;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class ComparatorLambda {
+
+    //Anonymous class implementation of the Comparator interface.
+    Comparator<String> comparator = new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+            return Integer.compare(o1.length(),o2.length());
+        }
+    };
+
+    //lambda expression implementation of the Comparator interface.
+    Comparator<String> comparatorLambda = (o1,o2) -> Integer.compare(o1.length(),o2.length());
+
+    {
+        List<String> list = Arrays.asList("*","****","**","*********","******");
+        //Collections.sort(list,comparator);
+        Collections.sort(list,comparatorLambda);
+        for (String l :list) {
+            System.out.println(l);
+        }
+    }
+
+
+}
